267258769a4cd030d2ba3c150bce7e082df1774b
Sylvie Salazar <sylvina_srosales@hotmail.com>
2025-10-06 14:26:13 -0600

    feat: Section 11 Udemy - Cold Observables

--- app/src/main/java/com/sylvieprojects/rxjavaapp/view/screens/coldAndHot/HotAndColdScreen.kt
+++ app/src/main/java/com/sylvieprojects/rxjavaapp/view/screens/coldAndHot/HotAndColdScreen.kt
@@ -1,13 +1,30 @@
 package com.sylvieprojects.rxjavaapp.view.screens.coldAndHot
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import android.util.Log
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import com.sylvieprojects.rxjavaapp.ui.theme.Purple40
+import androidx.compose.runtime.LaunchedEffect
+import io.reactivex.rxjava3.core.Observable
+import java.util.concurrent.TimeUnit
 
 @Composable
-fun ColdAndHotScreen(modifier: Modifier = Modifier) {
-    Column(modifier = modifier.fillMaxSize().background(color = Purple40)) {  }
+fun ColdAndHotScreen() {
+    LaunchedEffect(Unit) {
+        coldObservable()
+    }
+}
+
+private fun coldObservable() {
+    Log.d("Observable", "----------Cold----------")
+    val cold = Observable.interval(500, TimeUnit.MILLISECONDS).take(3)
+    cold.subscribe(
+        { e -> Log.d("Observable", "Subscriber 1: $e") }
+    )
+    try {
+        Thread.sleep(2000)
+    } catch (e: Exception) {
+
+    }
+    cold.subscribe(
+        { e -> Log.d("Observable", "Subscriber 2: $e") }
+    )
 }
